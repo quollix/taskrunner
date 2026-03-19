e7af0b3f1223e5379d677904513bfe55cd3042b4
Christoph Baier <anon@example.com>
2026-03-19 16:10:40 +0100

    refactoring

--- test.sh
+++ test.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-echo "building for linux"
-GOOS=linux GOARCH=amd64 go build
-echo "successfully built for linux"
-
-echo "building for windows"
-GOOS=windows GOARCH=amd64 go build
-echo "successfully built for windows"
-
+go build
 go test -tags=integration -count=1 ./...
